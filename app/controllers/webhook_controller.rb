ee6df42ea8c869bea577c070519936863a7dc8e1
nonomura-taku <nonomura.taku@dena.jp>
2016-05-15 20:50:11 +0900

    Fix undefined variable

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -56,7 +56,7 @@ class WebhookController < ApplicationController
   def is_validate_signature
     signature = request.headers["X-LINE-ChannelSignature"]
     http_request_body = request.raw_post
-    hash = OpenSSL::HMAC::digest(OpenSSL::Digest::SHA256.new, CHANNEL_SECRET, http_request_body)
+    hash = OpenSSL::HMAC::digest(OpenSSL::Digest::SHA256.new, ENV['LINE_CHANNEL_SECRET'], http_request_body)
     signature_answer = Base64.strict_encode64(hash)
     signature == signature_answer
   end
